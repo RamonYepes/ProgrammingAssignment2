786934af5d060ca7a157af41014aa5e8145fcab8
ryepes <ryepess@gmail.com>
2015-07-21 00:51:26 +0200

    Commit done for  peer evaluation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makes Cache matix defines a vector of functions to 
+## store and retrieve the inverse of a matrix 
 
-## Write a short comment describing this function
+## vector of functions to set  value of matrix m, get the value of m 
+##  set and get the value of im ( inverse of m)
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    m <<- y
+    im <<- NULL
+  }
+  get <- function() m
+  setinv <- function(m) im <<- solve(m)
+  getinv <- function() im
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## returns the value of the inverse if cached in parent environment 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m 
+  
 }
